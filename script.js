34319c7b4461377696f2e854ef17c5cc4b9347af
Jwhitmer23 <jonathanwhitmer@icloud.com>
2024-10-16 12:35:36 -0500

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function downloadData() {
+  event.preventDefult();
+  var startdate = document.getElementById("startdate").value;
+  var enddate = document.getElementById("enddate").value;
+  alert(startdate);
+  alert(enddate);
+}
